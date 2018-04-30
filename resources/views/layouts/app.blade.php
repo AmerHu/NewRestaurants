700851a20e2ce5d04945204f0611be729780d261
amerhu <amerhu@outlook.com>
2018-04-30 16:05:06 +0300

    fix style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
 
 </head>
-<body style="width: 98%">
+<body style="width: 98%;!important;">
 
 <aside id="left-panel" class="left-panel">
     <nav class="navbar navbar-expand-sm navbar-default">
